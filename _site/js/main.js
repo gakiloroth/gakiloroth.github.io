6645497796a4df9ddc0f630cae0f59c1fd946a65
gakiloroth <gakiloroth@yahoo.com>
2019-10-01 00:12:24 -0700

    added servant list filtering and appearing in dropdown functionality

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,3 +1,14 @@
 $("#inputClass").change(function(){
-  alert("class changed");
+  $('#maxGrailed').prop('disabled', true);
+  $('#maxGrailed').prop('checked', false);
+  $('#maxFou').prop('checked', false);
+  $('#inputNPLevel').val(1);
+  $('#hasNPupgrade').hide();
+  $('#inputServant').empty().append($('<option></option>').val('Select Servant').html('Select Servant'));
+  var matchClass = $("#inputClass option:selected").text();
+  servantList.filter(function(serv){
+    if(serv.class == matchClass){
+      $("#inputServant").append($('<option></option').val(serv.id).html(`${serv.id}: ${serv.name}`));
+    }
+  });
 });
---
+++ js/ce-list.js
@@ -0,0 +1,1 @@
+const ceList = [{"id":31,"name":"Formal Craft","attack":"500","superattack:"2000","npmultiplier":"","npstartcharge":"","npgeneration:"","busterup":"","artsup":"","quickup":""}]
